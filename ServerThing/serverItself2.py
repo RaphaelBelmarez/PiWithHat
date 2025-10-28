754ea955cd53e07e7e698665b4df4f04871bfa3b
Raphael Belmarez <raphbelmarez@hotmail.com>
2025-10-28 04:38:33 +0000

    python server work in progress

--- ServerThing/serverItself2.py
+++ ServerThing/serverItself2.py
@@ -3,17 +3,40 @@ import logging
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
-    def _set_response_(self):
+    def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_POST(self):
-        return 0
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        logging("POST request, \nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n", str(self.path), str(self.headers), post_data.decode('utf-8'))
+        self._set_response()
+        self.wfile.write("POST request {}".format(self.path).encode('utf-8'))
 
     def do_GET(self):
-        return 0
+        logging.info("GET request, \nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request {}".format(self.path).encode('utf-8'))
 
 
 def run(server_class=HTTPServer, handler_class=HTTPRequestHandler, port=8080):
-    return 0
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+    if __name__ == '__main__':
+        from sys import argv
+
+        if len(argv) == 2:
+            run(port=int(argv[1]))
+        else:
+            run()
